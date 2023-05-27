4aa7b65e96890e773db18bf1993f0c786277af06
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-27 17:28:58 +0300

    changed reg file

--- src/writeBack.java
+++ src/writeBack.java
@@ -14,23 +14,25 @@ public class writeBack {
 	public static void writeBack(String result, ArrayList<String> s) {
 		if (clock == 0)
 			return;
-		if (!pipelineIsEmpty() && clock%2!=0) {
-			
-				if (result == null) {
-					
-					inst.remove(ex);
-					++clock;
-					return;
-				} else {
-					if (!s.get(3).equals("0zer0"))
-					
-						registerFile[(Integer.parseInt(s.get(3)))] = Integer.parseInt(result);
-				}
+		if (!pipelineIsEmpty() && clock % 2 != 0) {
+
+			if (result == null) {
+
 				inst.remove(ex);
-				
 				++clock;
+				return;
+			} else {
+				if (!s.get(3).equals("0zer0")) {
+					System.out.println("R" + (Integer.parseInt(s.get(3)) - 1) + " changed from "
+							+ registerFile[(Integer.parseInt(s.get(3)))] + " to " + result);
+					registerFile[(Integer.parseInt(s.get(3)))] = Integer.parseInt(result);
+				}
 			}
-		
+			inst.remove(ex);
+
+			++clock;
+		}
+
 	}
 
 	public static boolean pipelineIsEmpty() {
